6cf27faa0e0be04137ad078998bfb73d920a7417
joec0der <45609323+joec0der@users.noreply.github.com>
2018-12-05 11:31:54 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,3 +3,9 @@ node default {
 node 'alcnt75201.va5e0lgt3b3uhkkeoammsvvf5a.cx.internal.cloudapp.net' {
   include role::master_server
 }
+node 'alcnt75202.va5e0lgt3b3uhkkeoammsvvf5a.cx.internal.cloudapp.net' {
+  include role::app_server
+}
+node 'alcnt75203.va5e0lgt3b3uhkkeoammsvvf5a.cx.internal.cloudapp.net' {
+  include role::db_server
+}
